09ab33d80b16c20b88fb5133fdb73855cec72f32
gavinyan123 <89555841+gavinyan123@users.noreply.github.com>
2022-03-08 20:46:15 -0500

    Add files via upload

--- kcluster.py
+++ kcluster.py
@@ -1,122 +1,95 @@
+#import libraries
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import random as rd
+import matplotlib.pyplot as plt
 
-class KCluster:
-    def __init__(self, file = None, name = None):
-        self.data = None
-        self.name = name
-        if file: 
-            self.data = pd.read_csv(file,index_col=False,header=None, names= name)
+# Select random observation as centroids
+def plot_init_centroids(data, K):
+    N = data.shape[0]
+    percentile_list  = [x for x in range(0,N,int(N/(K+1)))]    
+    Centroids = data.iloc[percentile_list[1:K+1]]
+    # Centroids = (data.sample(n=K))
+    # plt.scatter(data["rad"],X["compact"],c='black')
+    # plt.scatter(Centroids["rad"],Centroids["compact"],c='red')
+    # plt.xlabel('rad')
+    # plt.ylabel('compact')
+    # plt.show()
+    return Centroids
 
-    def plot_raw_data(self, malig, benign):
-        #Plot all the columns of the data against each other
-        for i in range(1,len(self.name),1):
-            x = self.name[i]
-            for j in range(i+1,len(self.name),1):
-                y = self.name[j]
-                X_mal = malig[[x,y]]
-                X_ben = benign[[x,y]]
-                #Visualise data points
-                plt.scatter(X_mal[x],X_mal[y],c='red')
-                plt.scatter(X_ben[x],X_ben[y],c='black')
-                #plt.imshow(X, cmap='hot', interpolation='nearest')
-                plt.xlabel(x)
-                plt.ylabel(f'{y}')
-                plt.show()
 
-    #Find initial centroids based on statistical averages and percentiles
-    def center(self, X, Y:int):
-        Z = self.data.shape[0]
-        p_list = [x for x in range(0, Z, int(Z/(Y+1)))]
-        return X.iloc[p_list[1:Y+1]]
+def clustering(data, col1, col2, K):
+    diff = 1
+    j=0
+    Centroids = plot_init_centroids(data, K)
+    X = data
+    elbow = []
+    total = 0
+    while(diff!=0):
+        XD=X
+        i=1
+        for index1,row_c in Centroids.iterrows():
+            ED=[]
+            for index2,row_d in XD.iterrows():
+                d1=(row_c[col1]-row_d[col1])**2
+                d2=(row_c[col2]-row_d[col2])**2
+                d=np.sqrt(d1+d2)
+                ED.append(d)
+            X[i]=ED
+            i=i+1
 
-    #implementation of KMeans
-    def KMeans(self, col1, col2, K):
-        Z = self.data.shape[0]
-        
-        x = col1
-        y = col2
-        X = self.data[[x,y]]
-        
-        #setting centroids with center method
-        centroids = self.center(X, K)
+        C=[]
+        for index,row in X.iterrows():
+            min_dist=row[1]
+            pos=1
+            for i in range(K):
+                if row[i+1] < min_dist:
+                    min_dist = row[i+1]
+                    pos=i+1
+            C.append(pos)
+        X["Cluster"]=C
+        Centroids_new = X.groupby(["Cluster"]).mean()[[col2,col1]]
+        if j == 0:
+            diff=1
+            j=j+1
+        else:
+            diff = (Centroids_new[col2] - Centroids[col2]).sum() + (Centroids_new[col1] - Centroids[col1]).sum()
+            print(diff.sum())
+        Centroids = X.groupby(["Cluster"]).mean()[[col2,col1]]
 
-        #plot centroids
-        plt.scatter(X[x], X[y],c='black')
-        plt.scatter(centroids[x], centroids[y], c='red')
-        plt.xlabel(x)
-        plt.ylabel(y)
-        plt.show()
+        for m in range(K):
+            k_sum = X[m+1].sum()
+        # for k in range(K):
+        #     color = np.random.rand(1,3)
+        #     data=X[X["Cluster"]==k+1]
+        #     plt.scatter(data[col1],data[col2],c=color)
+        # plt.scatter(Centroids[col1],Centroids[col2],c='red')
+        # plt.xlabel(col1)
+        # plt.ylabel(col2)
+        # plt.show()
+    elbow.append(k_sum)
+    return elbow
 
-        #Assign points to the closest centroid
-        #Recompute Centroids
-        #Repeat
-
-        diff = 1
-        j = 0
-
-        while(diff!=0):
-            XD = X
-            i=1
-            # for each centroid
-            for index1,row_c in centroids.iterrows():
-                ED=[]
-                #measure distance from centroid to every point
-                for index2,row_d in XD.iterrows():
-                    d1=(row_c[x]-row_d[x])**2
-                    d2=(row_c[y]-row_d[y])**2
-                    d=np.sqrt(d1+d2)
-                    ED.append(d)
-
-                X[i]=ED # store distance vector as part of original Matrix
-                i=i+1
-
-            C=[]
-            #find nearest centroid for each point
-            for index,row in X.iterrows():
-                min_dist=row[1]
-                pos=1
-                for i in range(K):
-                    if row[i+1] < min_dist:
-                        min_dist = row[i+1]
-                        pos=i+1
-                C.append(pos)
-            X["Cluster"]=C
-            Centroids_new = X.groupby(["Cluster"]).mean()[[y,x]]
-            if j == 0:
-                diff=1
-                j=j+1
-            else:
-                diff = (Centroids_new[y] - centroids[y]).sum() + (Centroids_new[x] - centroids[x]).sum()
-                print(diff.sum())
-            centroids = X.groupby(["Cluster"]).mean()[[y,x]]
-
-        for k in range(K):
-            color = np.random.rand(1,3)
-            print(color)
-            data=X[X["Cluster"]==k+1]
-            plt.scatter(data[x],data[y],c=color)
-        plt.scatter(centroids[x], centroids[y], c="red")
-        plt.xlabel(x)
-        plt.ylabel(y)
-        plt.show()
-
-if __name__ == "__main__":
-    names = ['id_num', 'outcome', 'rad', 'texture', 'perim', 'area', 'smooth', 'compact', 'concave', 'concave_points',
+names_n = ['id_num', 'outcome', 'rad', 'texture', 'perim', 'area', 'smooth', 'compact', 'concave', 'concave_points',
             'sym', 'fractal_dim', \
             'rad_SE', 'texture_SE', 'perim_SE', 'area_SE', 'smooth_SE', 'compact_SE', 'concave_SE',
             'concave_points_SE', 'sym_SE', 'fractal_dim_SE', \
             'rad_worst', 'texture_worst', 'perim_worst', 'area_worst', 'smooth_worst', 'compact_worst',
             'concave_worst', 'concave_points_worst', 'sym_worst', 'fractal_dim_worst']
-    
-    file = 'wdbc.data'
 
-    k = KCluster(file, names)
-    k.data['outcome'] = k.data['outcome'].map(lambda diag: bool(diag == "M"))  # M being cancerous
-    #choose a column to sort the data by, this makes it easier to pick initial centroil positions
-    k.data.sort_values( by=['area'], inplace=True)
+df = pd.read_csv('wdbc.data', index_col=False,header=None, names= names_n)
+df.head()
 
-    for K in range(2,10,1):
-        k.KMeans('rad', 'compact', K)
+X = df[["rad","compact"]]
+#Visualise data points
+# plt.scatter(X["rad"],X["compact"],c='black')
+# plt.xlabel('rad')
+# plt.ylabel('compact')
+# plt.show()
 
+#number of clusters
+plot_elbow = []
+for K in range(2,10,1):
+    plot_elbow.append(clustering(X, 'rad', 'compact', K))
+
+print(plot_elbow)
