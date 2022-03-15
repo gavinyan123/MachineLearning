a04eb723b5719e232b22ff7703fbca2b0f3ebd51
gavinyan123 <89555841+gavinyan123@users.noreply.github.com>
2022-03-15 01:13:14 -0400

    Add files via upload

--- kcluster.py
+++ kcluster.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
-import numpy.matlib
-import KMeansClustering
 
 # Select random observation as centroids
 def get_init_centroids(data, K):
@@ -21,19 +19,19 @@ def get_init_centroids(data, K):
 
 def plot_init_centroids(data, K):
     Centroids = get_init_centroids(data, K)
-    plt.scatter(data["rad"],X["compact"],c='black')
+    plt.scatter(data["rad"],data["compact"],c='black')
     plt.scatter(Centroids["rad"],Centroids["compact"],c='red')
     plt.xlabel('rad')
     plt.ylabel('compact')
     plt.show()
 
-def clustering(data, col1, col2, K):
+def clustering(data, col1, col2, K, plot):
     diff = 1
     j=0
     Centroids = get_init_centroids(data, K)
     X = data
-    elbow = []
-    total = 0
+    elbow_plot = []
+    
     while(diff!=0):
         XD=X
         i=1
@@ -56,25 +54,47 @@ def clustering(data, col1, col2, K):
                     pos=i+1
             C.append(pos)
         X["Cluster"]=C
-        cluster = np.array(X['Cluster'])
+
+        #find inertia/elbow method
+        z=0
+        elbow = []
+        elbow_sum = []
+
+        for index3, row_e in Centroids.iterrows():
+            centroid_point = Centroids.iloc[0]
+            xd = X.loc[df['Cluster'] == z+1]
+            for index4, row_f in xd.iterrows():
+                sum1 = (centroid_point[col1] - row_f[col1])**2
+                sum2 = (centroid_point[col2] - row_f[col2])**2
+                total = np.sqrt(sum1+sum2)
+                elbow.append(total)
+            elbow_sum.append(sum(elbow))
+            z+1
+        elbow_plot.append(elbow_sum[0])
+
         Centroids_new = X.groupby(["Cluster"]).mean()[[col2,col1]]
         if j == 0:
             diff=1
             j=j+1
         else:
             diff = (Centroids_new[col2] - Centroids[col2]).sum() + (Centroids_new[col1] - Centroids[col1]).sum()
-            print(diff.sum())
+            # print(diff.sum())
         Centroids = X.groupby(["Cluster"]).mean()[[col2,col1]]
 
-        for k in range(K):
-            color = np.random.rand(1,3)
-            data=X[X["Cluster"]==k+1]
-            plt.scatter(data[col1],data[col2],c=color)
-        plt.scatter(Centroids[col1],Centroids[col2],c='red')
-        plt.xlabel(col1)
-        plt.ylabel(col2)
-        plt.show()
-
+        if(plot ==1):
+            for k in range(K):
+                color = np.random.rand(1,3)
+                data=X[X["Cluster"]==k+1]
+                plt.scatter(data[col1],data[col2],c=color)
+            plt.scatter(Centroids[col1],Centroids[col2],c='red')
+            plt.xlabel(col1)
+            plt.ylabel(col2)
+            plt.show()
+    elbow_points = elbow_plot[:10]
+    plt.plot(range(len(elbow_points)), elbow_points,'go--', linewidth=1.5, markersize=4)
+    plt.xlabel("Num Clusters(K)")
+    plt.ylabel("Sum Squares")
+    plt.show()
 names_n = ['id_num', 'outcome', 'rad', 'texture', 'perim', 'area', 'smooth', 'compact', 'concave', 'concave_points',
             'sym', 'fractal_dim', \
             'rad_SE', 'texture_SE', 'perim_SE', 'area_SE', 'smooth_SE', 'compact_SE', 'concave_SE',
@@ -87,16 +107,9 @@ df['outcome'] = df['outcome'].map(lambda diag: bool(diag == "M"))  # M being can
 #choose a column to sort the data by, this makes it easier to pick initial centroil positions
 df.sort_values( by=['area'], inplace=True)
 
-K=4
+K=10
 
-X = df[["rad","compact"]]
-elbow = KMeansClustering
-cluster_array = np.array(X)
-cluster_vars = []
-centroids = [cluster_array[i+2] for i in range(K)]
-clusters = elbow.assign_clusters(centroids, cluster_array)
-initial_clusters = clusters
-X['cluster'] = clusters
+X=df
 
 #Visualise data points
 plt.scatter(X["rad"],X["compact"],c='black')
@@ -104,26 +117,7 @@ plt.xlabel('rad')
 plt.ylabel('compact')
 plt.show()
 
-for i in range(10):
-    centroids = elbow.calc_centroids(clusters, cluster_array)
-    clusters = elbow.assign_clusters(centroids, cluster_array)
-    cluster_var = np.mean(elbow.calc_centroid_variance(clusters, 
-                                                cluster_array))
-    cluster_vars.append(cluster_var)
-    # sumsq.append(sum(calc_centroid_variance(clusters, cluster_array)))
-    # print(i+1, round(cluster_var))
-x_label = []
-for i in range(len(cluster_vars)):
-    x_label.append(i+1)
-
-#plot elbow method
-#4/5 is optimal
-plt.plot(x_label, cluster_vars,'go--', linewidth=1.5, markersize=4)
-plt.xlabel("Num Clusters(K)")
-plt.ylabel("Sum Squares")
-plt.show()
-
 #iterate through cluster K amount of times
 for k in range(2,K,1):
     plot_init_centroids(X, k)
-    clustering(X, 'rad', 'compact', k)
+    clustering(X, 'rad', 'compact', k,1)
